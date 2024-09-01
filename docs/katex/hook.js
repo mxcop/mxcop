b01d75552d35d58ca87cfa1d7e27fc59c87f4b9b
MΛX <mxcop.dev@gmail.com>
2024-09-01 22:19:21 +0200

    ci: first build

---
+++ docs/katex/hook.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () =>
+    renderMathInElement(document.body, {
+      delimiters: [
+        { left: "$$", right: "$$", display: true },
+        { left: "$", right: "$", display: false },
+      ],
+      throwOnError: false,
+    }),
+);
